c40d6b793ee6d0ef3f7b332bf896079ac8fd647c
IgorBiesiada <xxxigorrbiesiada1@gmail.com>
2026-04-10 19:59:04 +0200

    docker and forms

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,40 +1,28 @@
 from django import forms
-from . models import SportResource, BeautyResource
+from .models import SportResource, OfficeResource, EventResource, BeautyResource, LivingResource
 
 
-base_fields = ['name', 'equipment', 'price_per_hour', 'address', 'city', 'description']
-
-
-class SportFrom(forms.ModelForm):
+class SportForm(forms.ModelForm):
     class Meta:
         model = SportResource
-
-        fields = [
-            'sport_type', 'surface', 'is_outside', 
-            'capacity'
-        ] + base_fields
-
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nazwa obiektu'}),
-            'sport_type': forms.Select(attrs={'class': 'form-select'}), 
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'price_per_hour': forms.NumberInput(attrs={'class': 'form-control'}),
-            'is_outside': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            
-        }
-
-        labels = {
-            'sport_type': 'Wybierz dyscyplinę sportu',
-            'is_outside': 'Czy obiekt jest na zewnątrz?'
-        }
+        fields = "__all__"
 
 class OfficeForm(forms.ModelForm):
     class Meta:
-        model = SportResource
+        model = OfficeResource
+        fields = "__all__"
 
-        fields = [
-             'desk', 'meeting_rooms', 'has_wifi', 'has_parking'] + base_fields
-        
-        widgets = {
-            'name' : forms.TextInput(attrs={'class': 'form-control'})
-        }
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = EventResource
+        fields = "__all__"
+
+class BeautyForm(forms.ModelForm):
+    class Meta:
+        model = BeautyResource
+        fields = "__all__"
+
+class LivingForm(forms.ModelForm):
+    class Meta:
+        model = LivingResource
+        fields = "__all__"
