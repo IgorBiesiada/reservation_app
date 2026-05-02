b29f988982a2e628913285282fde7f24e0d2f5fe
IgorBiesiada <xxxigorrbiesiada1@gmail.com>
2026-05-02 18:17:12 +0200

    login logout register views urls

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import CreateUserView, LoginUserView
+from . views import CreateUserView, LoginUserView, logout_view
 
 
 app_name = 'users'
@@ -7,5 +7,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
-    path('login/', LoginUserView.as_view(), name='login')
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout')
 ]
